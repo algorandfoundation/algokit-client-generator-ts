07804f5ec1c96cac463da34e14d5be03af9cec04
Tristan Menzel <tristanmenzel@gmail.com>
2023-05-24 17:37:00 -0700

    build: add a test that confirms readonly methods do not consume algos

--- examples/state/client.spec.ts
+++ examples/state/client.spec.ts
@@ -1,6 +1,7 @@
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing'
 import { beforeEach, describe, expect, test } from '@jest/globals'
 import { StateAppClient } from './client'
+import { microAlgos } from '../../../algokit-utils-ts/dist'
 
 describe('state typed client', () => {
   const localnet = algorandFixture()
@@ -37,5 +38,32 @@ describe('state typed client', () => {
     expect(localState.local_int2?.asNumber()).toBe(2)
     expect(localState.local_bytes1?.asString()).toBe('asdf')
     expect(localState.local_bytes2?.asByteArray()).toEqual(new Uint8Array([1, 2, 3, 4]))
+  })
+
+  test('Readonly methods do not consume algos', async () => {
+    const { algod, indexer, testAccount } = localnet.context
+    const client = new StateAppClient(
+      {
+        resolveBy: 'creatorAndName',
+        sender: testAccount,
+        creatorAddress: testAccount.addr,
+        findExistingUsing: indexer,
+      },
+      algod,
+    )
+    await client.deploy({ deployTimeParams: { VALUE: 1 } })
+
+    const minBalance = 10_000
+    const txCost = 1_000
+
+    const lowFundsAccount = await localnet.context.generateAccount({ initialFunds: microAlgos(minBalance + txCost) })
+
+    const result = await client.callAbi({ value: 'oh hi' }, { sender: lowFundsAccount })
+
+    expect(result.return).toBe('Hello, oh hi')
+
+    // If we can invoke this method twice it confirms that we are still above the min balance + single tx amount and the previous call
+    // did not consume algos
+    await client.callAbi({ value: 'oh hi' }, { sender: lowFundsAccount })
   })
 })
