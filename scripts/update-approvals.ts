ab85fea31904608fbf10a6e82b6a01dcdb335484
Rob Moore (MakerX) <rob.moore@makerx.com.au>
2024-09-28 17:55:40 +0800

    Added latest reti contracts and test

--- scripts/update-approvals.ts
+++ scripts/update-approvals.ts
@@ -3,13 +3,30 @@ import * as path from 'path'
 import { colorConsole } from '../src/util/color-console'
 
 function updateApprovals() {
-  const dirs = ['helloworld', 'lifecycle', 'minimal', 'state', 'voting', 'duplicate_structs', 'minimal', 'reti', 'arc56_test'].map((app) =>
-    path.join(process.cwd(), 'examples', app),
-  )
+  const dirs = [
+    'helloworld',
+    'lifecycle',
+    'minimal',
+    'state',
+    'voting',
+    'duplicate_structs',
+    'minimal',
+    'reti1',
+    'reti2',
+    'arc56_test',
+  ].map((app) => path.join(process.cwd(), 'examples', app))
 
   for (const exampleDir of dirs) {
-    const generatedPath = path.join(exampleDir, 'client.generated.ts')
-    const approvedPath = path.join(exampleDir, 'client.ts')
+    const generatedPath = exampleDir.includes('reti1')
+      ? path.join(exampleDir.substring(0, exampleDir.length - 1), 'validator.client.generated.ts')
+      : exampleDir.includes('reti2')
+        ? path.join(exampleDir.substring(0, exampleDir.length - 1), 'staking.client.generated.ts')
+        : path.join(exampleDir, 'client.generated.ts')
+    const approvedPath = exampleDir.includes('reti1')
+      ? path.join(exampleDir.substring(0, exampleDir.length - 1), 'validator.client.ts')
+      : exampleDir.includes('reti2')
+        ? path.join(exampleDir.substring(0, exampleDir.length - 1), 'staking.client.ts')
+        : path.join(exampleDir, 'client.ts')
 
     if (!fs.existsSync(generatedPath)) {
       colorConsole.error`Need to run ${'npm run dev'} task first to generate latest clients. Could not find file ${generatedPath}`
